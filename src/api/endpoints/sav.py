57919dac08719c473909264e05672c4e89edcd77
pierre-jannot <pierre.jannot@isen.yncrea.fr>
2026-09-15 15:13:18 +0200

    [BACKEND] Ajout d'une route avec éléments filtrés
    Ajout d'une route permettant de choisir des éléments de filtrage pour préciser le modèle ayant des problèmes et le code erreur.

--- src/api/endpoints/sav.py
+++ src/api/endpoints/sav.py
@@ -6,8 +6,7 @@ from fastapi import APIRouter, HTTPException
 
 from src.api.schemas import AnswerResponse, QuestionRequest, DetailedQuestionRequest
 from src.client.groq_client import GroqConfigError, GroqRequestError
-from src.services.call.ask_service import ask
-from src.services.call.ask_service import ask
+from src.services.call.ask_service import ask, ask_filtered
 
 router = APIRouter(prefix="/sav", tags=["sav"])
 
@@ -32,14 +31,14 @@ def ask_question(request: QuestionRequest) -> AnswerResponse:
     return AnswerResponse(answer=answer)
 
 
-@router.post("/ask-detailed")
-def ask_question(request: DetailedQuestionRequest) -> AnswerResponse:
+@router.post("/ask-filtered")
+def ask_question_filtered(request: DetailedQuestionRequest) -> AnswerResponse:
     """
     Reçoit une question utilisateur avec des where_clause, exécute le pipeline RAG complet
     et renvoie la réponse.
     """
     try:
-        answer = ask(request.question, request.parametres)
+        answer = ask_filtered(request.question, request.appliance_type, request.model_id, request.error_code)
     except GroqConfigError as e:
         raise HTTPException(
             status_code=500, detail="Erreur de configuration du service LLM."
